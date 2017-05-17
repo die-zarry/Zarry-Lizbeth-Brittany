96a80a5cc8615b45ad973374f37ab6687d11747c
Lizbethn <cas13065@byui.edu>
2017-05-17 09:50:09 -0500

    This was our first version of our project

---
+++ obesity_v1/README.md
@@ -0,0 +1,1 @@
+# Zarry-Lizbeth-Brittany
---
+++ obesity_v1/src/byui/cit260/obesity/model/RessourcesScene.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.obesity.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class RessourcesScene implements Serializable{
+    
+    private double amount;
+    private String type;
+
+    public RessourcesScene() {
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + (int) (Double.doubleToLongBits(this.amount) ^ (Double.doubleToLongBits(this.amount) >>> 32));
+        hash = 97 * hash + Objects.hashCode(this.type);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "RessourcesScene{" + "amount=" + amount + ", type=" + type + '}';
+    }
+
+    
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RessourcesScene other = (RessourcesScene) obj;
+        if (Double.doubleToLongBits(this.amount) != Double.doubleToLongBits(other.amount)) {
+            return false;
+        }
+        if (!Objects.equals(this.type, other.type)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
